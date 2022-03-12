e5528d66352dd77fff192246be7d7b69d61e87b1
HEINRICK7 <heinrickcostta@gmail.com>
2022-03-12 09:29:19 -0300

    upddate pages

--- pokedex/src/pages/ListPokemonsPage/index.jsx
+++ pokedex/src/pages/ListPokemonsPage/index.jsx
@@ -7,50 +7,42 @@ import { useNavigate } from 'react-router-dom'
 import { Container, Card, Button } from './styled'
 
 function ListPokemonsPage() {
-  const [pokemons] = useContext(GlobalStateContext)
-  const [ pokedex, setPokedex ] = useContext(GlobalStateContext);
-  const [pokemonList, setPokemonList] = useContext(GlobalStateContext);
-  const [newPokemons, setNewPokemons] = useState(pokemonList);
+  const {pokemons, setPokemons, pokedex, setPokedex, paginacao, setPaginacao, pokemonList, setPokemonList}=useContext(GlobalStateContext)
 
   const history = useNavigate()
 
-  const addToPokedex = (pokemonToAdd) => {
-    console.log("entrou");
+  const onClickAdd = ((pokeToAdd)=>{
+    
+    const index = pokemonList.findIndex((pokemon)=>{
+        return pokemon.name === pokeToAdd.name
+    })
 
-    const position = pokedex.findIndex((pokemon) => {
-      return pokemon.id === pokemonToAdd.id;
-    });
+    if(index === -1){
+        const listaPokes = pokemonList
+        listaPokes.push(pokeToAdd)
+        setPokemonList(listaPokes)
+        console.log(pokemonList)
 
-    console.log(position);
-  
-  const newPokedex = [...pokedex];
-
-    console.log('list',newPokedex);
-
-    if (position === -1) {
-      newPokedex.push({ ...pokemonToAdd });
+        const newPokemonList = pokemons.filter((pokemon)=>{ //removendo da home
+          return pokemon.name !== pokeToAdd.name
+      })
+      setPokemons(newPokemonList)
+      localStorage.setItem('pokedex', JSON.stringify(pokemonList))
+      localStorage.setItem('pokemons', JSON.stringify(pokemons))
     }
 
-    setPokedex(newPokedex);
-  
-    const newPokemonList = pokemons.filter(
-      (pokemon) => pokemon.id !== pokemonToAdd.id
-    );
-
-    setNewPokemons(newPokemonList);
-  };
-  console.log('new',newPokemons)
+})
   return (
     <>
       <HeaderList />
       <Container>
         {
           pokemons.map(res => (
-            <Card>
+            <Card key={res.id}>
                 <p>{res.name[0].toUpperCase() + res.name.slice(1)}</p>
                 <img src={res.image} alt={res.name} style={{width: '50%'}}/>
               <div>
-                <Button onClick={() => {addToPokedex(res)}}>Adicionar</Button>
+                <Button onClick={() => {onClickAdd(res)}}>Adicionar</Button>
                 <Button onClick={() => history(`/details_pages/${res.name}`)}>Detalhes</Button>
               </div>
             </Card>))
