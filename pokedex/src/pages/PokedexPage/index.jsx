a609d3509312da23cbd02aa08fb569d3ec48999d
jaime-github <jaime-epifanio@hotmail.com>
2022-03-12 16:33:21 -0300

    Tela Pokedex

--- pokedex/src/pages/PokedexPage/index.jsx
+++ pokedex/src/pages/PokedexPage/index.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useContext } from "react";
+import HeaderPokedex from "../../components/HeaderPokedex";
+import GlobalStateContext from "../../context/GlobalContext/GlobalStateContext";
+import { Container, Card, Button } from "./styled";
+import { useNavigate } from 'react-router-dom'
 
-// import { Container } from './styles';
 
 function PokedexPage() {
+  const {
+    pokemons,
+    pokedex,
+    pokemonList,
+    setPokemonList,
+    setPokemons,
+    setPokedex,
+  } = useContext(GlobalStateContext);
+
+  const history = useNavigate()
+  
+  const onClickRemover = (RemovePoke,res) => {
+    const novaLista = pokemonList.filter((pokemon) => {
+      localStorage.removeItem(res.id);
+      return pokemon.name !== RemovePoke.name;
+    });
+    setPokemonList(novaLista);
+    const retornaPokemon = [RemovePoke, ...pokemons];
+    setPokemons(retornaPokemon);
+  };
+  const pokes = localStorage.getItem("pokedex");
+  const poker = JSON.parse(pokes);
+  console.log("store", poker);
   return (
-      <h1>Hello Pokedex</h1>
+    <>
+      <HeaderPokedex />
+      <Container>
+        {poker.map((res) => (
+          <Card key={res.id}>
+            <p>{res.name[0].toUpperCase() + res.name.slice(1)}</p>
+            <img src={res.image} alt={res.name} style={{ width: "50%" }} />
+            <div>
+              <Button onClick={() => {onClickRemover(res)}}>Remover</Button>
+              <Button onClick={() => history(`/details_pages/${res.name}`)}>Detalhes</Button>
+            </div>
+          </Card>
+        ))}
+      </Container>
+    </>
   );
 }
 
