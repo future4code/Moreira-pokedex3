618af4015211bd18c54d0aa41382fbf848ff93ff
HEINRICK7 <heinrickcostta@gmail.com>
2022-03-12 09:28:17 -0300

    upddate GlobalState

--- pokedex/src/context/GlobalContext/GlobalState.js
+++ pokedex/src/context/GlobalContext/GlobalState.js
@@ -1,32 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import GlobalStateContext from "./GlobalStateContext"
-
 const GlobalState = (props) => {
     const [pokemons, setPokemons] = useState([])
-    const { pokemonList, setPokemonList } = useState([]);
     const [pokedex, setPokedex] = useState([])
+    const [pokemonList, setPokemonList] = useState([])
     const [loading, setLoading] = useState(true)
+    const [paginacao, setPaginacao] = useState(0)
 
     useEffect(() => {
-       
-           axios.get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
-           .then(response => 
+           axios.get(`https://pokeapi.co/api/v2/pokemon?limit=21&offset=${paginacao}`)
+           .then(response =>
             pokemonData(response.data.results),
             )
             .catch((error) => {
                 console.log(error.response.message)
             })
-            setLoading(false) 
-
-    }, []);
-
+            setLoading(false)
+    }, [paginacao]);
     const pokemonData = async (data) => {
         const loadingPokemon = await Promise.all(
             data.map(async(pokemon) => {
                 const pokemonRecord = await axios.get(pokemon.url)
                 .then((response) => response.data
-                
                 )
                 .catch((error) => {
                     console.log(error)
@@ -35,20 +31,16 @@ const GlobalState = (props) => {
                 return {
                     ...pokemonRecord,
                     image
-
-
                 }}),
-               
         );
         setPokemons(loadingPokemon);
-    
     }
-    
+
+   
     return (
-        <GlobalStateContext.Provider value={[pokemons, pokedex, setPokedex, pokemonList, setPokemonList]}>
+        <GlobalStateContext.Provider value={{pokemons, pokedex, paginacao, setPaginacao, setPokemons, setPokedex, pokemonList, setPokemonList}}>
             {props.children}
-        </GlobalStateContext.Provider>
+        </GlobalStateContext.Provider> 
     )
 }
-
 export default GlobalState
